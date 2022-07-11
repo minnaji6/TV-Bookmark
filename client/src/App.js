3358ef0c20444ed960902366c903b1114fb7f5ab
Tosie <minnaji@outlook.com>
2022-07-11 10:36:40 -0400

    dark/light mode start redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,14 @@
 import './App.css';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
+import HomeLight from './Componets/HomeLight';
+
+
 
 
 function App() {
   return (
     <>
-    <div className="App">
-
-<br/>
-<div className="logo">
-<img src="https://thumbs4.imagebam.com/37/ed/85/MEBNMNV_t.png" alt="LOGO"/>
-</div>
-<br/>
-
-<div className="App-nav">
-<ButtonGroup variant="contained" aria-label="outlined primary button group">
-  <Button>HOME</Button>
-  <Button>VIEW SHOWS</Button>
-  <Button>EDIT SHOWS</Button>
-</ButtonGroup>
-
-</div>
-
-
-<br/>
-<br/>
-<div className="App-body">
-
-  <div className="form2">
-
-  <input type="text" placeholder="SEARCH SHOWS" />
-  <br/>
-
-  <Button variant="contained">SEARCH</Button>
-
-  </div>
-
-  <div className="TAGS">
-  
-
-  </div>
-
-  <br/> 
-  
-  <h5>Select tag from dropdown</h5>
-
-  <div className="tag">
-  <br/>
-
-
-  <select> 
-    <option value="1">tag1</option>
-    <option value="2">tag2</option>
-    <option value="3">tag3</option>
-  </select>
-
-  {/* render shows based on tag here */}
-  <br/>
-  <br/>
-
-  </div>
-
-
-
-</div>
-
-</div>
-    </>
+    <HomeLight/>
+     </>
   );
 }
 
