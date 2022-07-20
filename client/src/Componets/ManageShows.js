9c08ed5f6a38ce394eea398de578a63a914464c3
Tosie <minnaji@outlook.com>
2022-07-20 05:47:36 -0400

    css changes

--- client/src/Componets/ManageShows.js
+++ client/src/Componets/ManageShows.js
@@ -11,42 +11,7 @@ function ManageShows() {
     <div className="App-body">
       
 
-    <div className="form2">
-
-      <input type="text" placeholder="SEARCH SHOWS" />
-      <br/>
-
-      <Button variant="contained"><SearchIcon/>SEARCH</Button>
-
-      </div>
-
-      <div className="TAGS">
-      
-
-      </div>
-
-      <br/> 
-      
-      <h5>Select Show tag from dropdown</h5>
-
-      <div className="tag">
-      <br/>
-
-
-      <select> 
-        <option value="1">ongoing</option>
-        <option value="2">cancelled</option>
-        <option value="3">upcoming</option>
-        <option value="4">finished</option>
-        <option value="5">watching</option>
-        <option value="6">want to watch</option>
-      </select>
-
-      {/* render shows based on tag here */}
-      <br/>
-      <br/>
-
-      </div>
+    
 
       <FetchCardDetails />
      
