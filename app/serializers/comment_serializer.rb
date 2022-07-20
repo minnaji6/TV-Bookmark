581c574f1162d2367dfef5251c9cac166a26711f
Tosie <minnaji@outlook.com>
2022-07-19 23:21:15 -0400

    showlist added to watchlist page

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,11 +1,11 @@
 class CommentSerializer < ActiveModel::Serializer
 
   attributes :id, :description, :show_id, :tag_id, :show_name, :show_image
-
   def show_name
     object.show.name
   end
   def show_image
     object.show.image
   end
+
 end
--- client/src/Componets/Watchlist.js
+++ client/src/Componets/Watchlist.js
@@ -1,20 +1,28 @@
 import React, { useState, useEffect} from 'react'
 import axios from 'axios'
-import { Grid, Card, CardMedia, CardContent, Typography, CardActions, Button }  from "@mui/material";
+import { Grid, Card, CardMedia, CardContent, Typography, CardActions, Chip }  from "@mui/material";
 
 
 
 const Watchlist = () => {
     const [cards, setCards] = useState([])
+    const [showlist, setShowlist] = useState([])
 
     useEffect(() => {
         axios.get(`http://localhost:3000/wishlist`)
         .then(res => setCards(res.data))
         
     }, [])
+
+    useEffect(() => {
+        axios.get(`http://localhost:3000/comments`)
+        .then(res => setShowlist(res.data))
+        
+    }, [])
     
     return (
         <>
+        <h2>Watchlist</h2>
         <Grid container spacing={3}>
             {cards.map(card => (
                 <Grid item xs={12} sm={6} md={3} key={card.id}>
@@ -32,6 +40,27 @@ const Watchlist = () => {
                 </Grid>
             ))}
         </Grid>
+       <h3>Shows sorted by tags</h3>
+        <Grid container spacing={3}>
+            {showlist.map(card => (
+                <Grid item xs={12} sm={6} md={3} key={card.id}>
+                    <Card>
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {card.show_name}
+                            </Typography>
+
+                            <Typography variant="body2" color="textSecondary" component="p">
+                             {card.tag_id === 1 ? 'ONGOING' : card.tag_id === 2 ? 'CANCELLED' : card.tag_id === 3 ? 'UPCOMING' : card.tag_id === 4 ? 'FINISHED'  : card.tag_id === 5 ? 'WATCHING' : 'WANT TO WATCH'}
+
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            ))}
+        </Grid>
+
+
         
         </>
     )
